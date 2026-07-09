c15f0b38f75ebcd1b893fdc84b4984cbb4acd465
Colin Wallace <cwallace@qumulo.com>
2026-07-09 16:48:40 +0000

    std: replace the unix fd implementation with the qumulo version

--- library/std/src/sys/fd/mod.rs
+++ library/std/src/sys/fd/mod.rs
@@ -3,7 +3,10 @@
 #![forbid(unsafe_op_in_unsafe_fn)]
 
 cfg_select! {
-    any(target_family = "unix", target_os = "wasi") => {
+    target_env = "qumulo" => {
+        pub use crate::qumulo::fd::*;
+    }
+    all(any(target_family = "unix", target_os = "wasi"), not(target_env = "qumulo")) => {
         mod unix;
         pub use unix::*;
     }
