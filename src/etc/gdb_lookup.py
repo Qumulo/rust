7b68f79c101fd96034020b422f6b424aafac943f
Eric Youngblut <ericy@qumulo.com>
2026-07-09 16:49:22 +0000

    Make the gdb plugin imports relative
    Qumulo symlinks the gdb plugins into site-python, so absolute imports
    of sibling modules do not resolve.

--- src/etc/gdb_lookup.py
+++ src/etc/gdb_lookup.py
@@ -2,8 +2,8 @@ import gdb
 import gdb.printing
 import re
 
-from gdb_providers import *
-from rust_types import *
+from .gdb_providers import *
+from .rust_types import *
 
 
 _gdb_version_matched = re.search("([0-9]+)\\.([0-9]+)", gdb.VERSION)
