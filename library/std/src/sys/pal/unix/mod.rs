1b435d28e5dff02dd58b221fdafc2335e60c2718
Stephen Fleischman <sfleischman@qumulo.com>
2026-07-09 16:48:40 +0000

    std: use qumulo replacements for the unix thread and thread_parking APIs

--- library/std/src/sys/pal/unix/mod.rs
+++ library/std/src/sys/pal/unix/mod.rs
@@ -1,8 +1,15 @@
-#![allow(missing_docs, nonstandard_style)]
+#![allow(missing_docs, nonstandard_style, unused_imports, dead_code)]
 
 use crate::io;
 
 pub mod conf;
+
+#[cfg(target_env = "qumulo")]
+pub use crate::qumulo::{
+    thread,
+    thread_parking,
+};
+
 #[cfg(target_os = "fuchsia")]
 pub mod fuchsia;
 pub mod futex;
@@ -10,6 +17,7 @@ pub mod futex;
 pub mod linux;
 pub mod stack_overflow;
 pub mod sync;
+#[cfg(not(target_env = "qumulo"))]
 pub mod thread_parking;
 pub mod time;
 pub mod weak;
